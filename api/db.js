d8d68ff75dc004dd07310a2272dd40ef79da1932
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2024-12-20 15:43:45 -0500

    set up turso db

---
+++ api/db.js
@@ -0,0 +1,11 @@
+import { createClient } from '@libsql/client'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN
+})
+
+export default client
