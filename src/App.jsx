a5eda026ef420e32bf9f35e0a13ff4d324584937
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2025-02-16 15:13:06 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 const router = createRouter({ routeTree });
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -12,6 +14,16 @@ const queryClient = new QueryClient({
 });
 
 export default function App() {
+  const [isRouterReady, setIsRouterReady] = useState(false);
+
+  useEffect(() => {
+    router.load().then(() => setIsRouterReady(true));
+  }, []);
+
+  if (!isRouterReady) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App, { router } from './App'
-
-// Wait for the router to be ready before rendering
-await router.load()
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
